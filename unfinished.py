c37065e55d8d5c1666e5e123a327f24e5c4756fe
Annna777 <50345202+Annna777@users.noreply.github.com>
2019-05-13 22:53:06 +0800

    Update unfinished.py

--- unfinished.py
+++ unfinished.py
@@ -4,7 +4,7 @@ f=open(r'C:\Users\79084\Desktop\text-file-process-Annna777\text-file-process\log
 
 l = list()
 for line in f:
-    line2=re.split(r':,',line)
+    line2=re.split(r'[:,]',line)
     l.append(line2[3])
 t=l.count(201811123026)
 print(t)
